127e6236d9668e280e3e2e6034d39ea6617cd73a
kid-kodi <konedangui@gmail.com>
2016-06-16 20:01:22 +0000

    commit 2

---
+++ app/src/main/java/com/alaqos/pointvente/activities/NotificationActivity.java
@@ -0,0 +1,24 @@
+package com.alaqos.pointvente.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.alaqos.pointvente.R;
+
+public class NotificationActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+
+        setContentView(R.layout.activity_help);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
